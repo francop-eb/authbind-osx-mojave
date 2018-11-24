3774ed5182c5518fea11ae54f5ce07d97353f1aa
francop-eb <francop@eventbrite.com>
2018-11-24 11:55:26 -0300

    finished configuration script

--- install_mojave.sh
+++ install_mojave.sh
@@ -3,5 +3,18 @@
 #Authbind compilation and install
 make
 sudo make install
+make clean
+#Configure authbind for the default osx user:
+name = echo $USER 
+sudo su
 
-#
+#Not happy with this solution:
+for i in `seq 1 1023`;
+do
+  touch /etc/authbind/byport/$i
+  chmod 500 /etc/authbind/byport/$i
+  chown ${name} /etc/authbind/byport/$i
+  touch /etc/authbind/byport/\!$i
+  chmod 500 /etc/authbind/byport/\!$i
+  chown ${name} /etc/authbind/byport/\!$i
+done
